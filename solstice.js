59a0574a36e56de9492a7cc36cadffc93e5b9361
CryroCat <44964856+CryroCat@users.noreply.github.com>
2020-12-04 21:18:14 -0800

    bodged some SHIT AND IT WOOORKKK WOOOOODOPSDFGBNHJKEDSGHJFVSHJNFB GVHJSDFBVJKH G

--- solstice.js
+++ solstice.js
@@ -2,7 +2,10 @@ var currentDate;
 var targetDate;
 var timeLeft, daysLeft, hoursLeft, minsLeft, secsLeft;
 var dots = 1;
-var timeZoneOffset = new Date().getTimezoneOffset(); // Returns Timezone offset in Minutes. ("How many minutes do you have to add or remove to reach UTC?")
+var timeZoneOffset = 480 // Returns Timezone offset in Minutes. ("How many minutes do you have to add or remove to reach UTC?")
+
+//var timeZoneOffset = new Date().getTimezoneOffset(); // Returns Timezone offset in Minutes. ("How many minutes do you have to add or remove to reach UTC?")
+
 
 function calculateTime(){
   currentDate = new Date();
@@ -92,13 +95,13 @@ function run(){
 
 $(document).ready(function(){
   $("#targetdate").datepicker({dateFormat: "YYYY MM DD"});
-  targetDate = new Date("2020 December 21");
+  targetDate = new Date("2020 December 21 24:00:00");
   targetDate.setMinutes(targetDate.getMinutes() - timeZoneOffset); // Compensates for any time zone.
   console.log (targetDate);
   $("#button").click(function() {
     $("#explanation").toggle();
   });
-  setInterval(run, 1000);
+  setInterval(run, 1000);	
 });
 //pain
 //dummy
