b2c936fb9b9868e25d131fb6b00ab1bcd816f008
CryroCat <44964856+CryroCat@users.noreply.github.com>
2020-12-05 16:58:00 -0800

    praise moment.js

--- solstice.js
+++ solstice.js
@@ -2,9 +2,13 @@ var currentDate;
 var targetDate;
 var timeLeft, daysLeft, hoursLeft, minsLeft, secsLeft;
 var dots = 1;
+
 //var timeZoneOffset = 0 // Returns Timezone offset in Minutes. ("How many minutes do you have to add or remove to reach UTC?")
 
 //var timeZoneOffset = new Date().getTimezoneOffset(); // Returns Timezone offset in Minutes. ("How many minutes do you have to add or remove to reach UTC?")
+
+
+var timeZoneOffset = new Date().getTimezoneOffset(); // Returns Timezone offset in Minutes. ("How many minutes do you have to add or remove to reach UTC?")
 
 
 function calculateTime(){
@@ -99,13 +103,17 @@ function run(){
 
 $(document).ready(function(){
   $("#targetdate").datepicker({dateFormat: "YYYY MM DD"});
+
   targetDate = new Date("2020 December 21 24:00:00");
   //targetDate.setMinutes(targetDate.getMinutes() - timeZoneOffset); // Compensates for any time zone.
+
+  targetDate = new Date("2020 December 21");
+  targetDate.setMinutes(targetDate.getMinutes() - timeZoneOffset); // Compensates for any time zone.
   console.log (targetDate);
   $("#button").click(function() {
     $("#explanation").toggle();
   });
-  setInterval(run, 1000);	
+  setInterval(run, 1000);
 });
 //pain
 //dummy
