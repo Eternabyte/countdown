19c3610c5c03b3138870f6771c059bf2ac9ad04b
CryroCat <44964856+CryroCat@users.noreply.github.com>
2020-12-05 17:47:03 -0800

    testtestetsteyasdsfsdafa

--- solstice2.js
+++ solstice2.js
@@ -4,7 +4,7 @@ window.onload = function(e){
     var $clock = $('#clock'),
         eventTime = moment('21-12-2020 15:30:00', 'DD-MM-YYYY HH:mm:ss').unix(),
         currentTime = moment().unix(),
-        diffTime = eventTime - currentTime,
+        diffTime = eventTime //- currentTime,
         duration = moment.duration(diffTime * 1000, 'milliseconds'),
         interval = 1000;
 
@@ -18,15 +18,19 @@ window.onload = function(e){
             $h = $('<div class="hours" ></div>').appendTo($clock),
             $m = $('<div class="minutes" ></div>').appendTo($clock),
             $s = $('<div class="seconds" ></div>').appendTo($clock);
-
+			
+			//TZ
+		var $clock = moment().tz("Canada/Vancouver").format();
+			
+			
         setInterval(function(){
-
+			
             duration = moment.duration(duration.asMilliseconds() - interval, 'milliseconds');
             var d = moment.duration(duration).days(),
                 h = moment.duration(duration).hours(),
                 m = moment.duration(duration).minutes(),
                 s = moment.duration(duration).seconds();
-
+				
             d = $.trim(d).length === 1 ? '0' + d : d;
             h = $.trim(h).length === 1 ? '0' + h : h;
             m = $.trim(m).length === 1 ? '0' + m : m;
